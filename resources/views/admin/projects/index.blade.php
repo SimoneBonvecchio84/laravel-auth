352641fd700dd0dd4024861bb771abd56d58e821
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-20 18:21:36 +0200

    Feat: added contents in table to index.blade

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,4 +2,26 @@
 
 @section('content')
     <h1>Index </h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Contenuto</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Azioni</th>   
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($projectsArray as $curProject)
+            <tr>
+              <th scope="row">{{ $curProject->id }}</th>
+              <td>{{ $curProject->title }}</td>
+              <td>{{$curProject->content}}</td>
+              <td>{{$curProject->slug}}</td>
+            </tr>
+                
+            @endforeach
+        </tbody>
+      </table>
 @endsection
